81cd370e962c523e41798111c92cd42181ae7bc2
Heros Henrique <HerosHenrique439@gmail.com>
2026-09-25 14:38:14 -0300

    remove arquivos duplicados

--- src/Bloco/bloco.module.ts
+++
@@ -1,9 +0,0 @@
-// blocos.module.ts
-import { Module } from '@nestjs/common';
-import { BlocosController } from './blocos.controller';
-import { BlocosService } from './blocos.service';
-@Module({
-  controllers: [BlocosController],
-  providers: [BlocosService]
-})
-export class BlocosModule {}
--- src/auth/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Papel } from '../../usuario/usuario.service';
-
-export const PAPEIS_KEY = 'papeis';
-export const Papeis = (...papeis: Papel[]) => SetMetadata(PAPEIS_KEY, papeis);
